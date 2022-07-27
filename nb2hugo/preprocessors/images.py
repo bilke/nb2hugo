0df3f825de9ba38def0b49f9fb661001419eb53e
Lars Bilke <lars.bilke@ufz.de>
2022-07-27 11:15:19 +0200

    Allow for images with #class and ""-captions.

--- nb2hugo/preprocessors/images.py
+++ nb2hugo/preprocessors/images.py
@@ -12,27 +12,32 @@ class ImagesPreprocessor(Preprocessor):
 
     def preprocess(self, nb, resources):
         """Preprocess the entire notebook."""
-        if not 'images_path' in resources:
-            resources['images_path'] = {}    
+        if not "images_path" in resources:
+            resources["images_path"] = {}
         for index, cell in enumerate(nb.cells):
             nb.cells[index], resources = self.preprocess_cell(cell, resources, index)
         return nb, resources
-    
+
     def preprocess_cell(self, cell, resources, index):
         """Preprocess one cell."""
         if cell.cell_type == "markdown":
             # Find and process links:
-            process_match = lambda m: self._process_image_link(m.group(1), m.group(2), resources)
-            cell.source = re.sub('!\[([^"]*?)\]\(([^"]+?)\)', process_match, cell.source)
+            process_match = lambda m: self._process_image_link(
+                m.group(1), m.group(2), resources
+            )
+            cell.source = re.sub('!\[([^"]*?)\]\((.+?)\)', process_match, cell.source)
         return cell, resources
-    
+
     def _process_image_link(self, alt_text, url, resources):
         """Copy image and return updated link."""
-        url_as_path = os.path.join(resources['metadata']['path'], url)
-        if os.path.isfile(url_as_path):
+        url_as_path = os.path.join(resources["metadata"]["path"], url)
+        result = re.search(r"(?:(?![#|\s]).)*", url_as_path)
+        match = result.group()
+        # print(f"{url_as_path} -> {match}")
+        if os.path.isfile(match):
             filename = os.path.basename(url)
-            resources['images_path'][filename] = url_as_path
-            link = '![' + alt_text + '](' + filename + ')'
+            resources["images_path"][filename] = match
+            link = "![" + alt_text + "](" + filename + ")"
         else:
-            link = '![' + alt_text + '](' + url + ')'
+            link = "![" + alt_text + "](" + url + ")"
         return link
